29c98affeee4ac99c9d2c3a95adbea9da37ca120
plombardi <plombardi89@gmail.com>
2017-09-06 10:05:45 -0400

    peg dependency versions

--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
+import os
 import versioneer
 
 from setuptools import setup, find_packages
+
+ROOT_DIR = os.path.dirname(__file__)
+
+with open(os.path.join(ROOT_DIR, "requirements.txt")) as fp:
+    install_requirements = [i.strip() for i in list(fp)
+                            if i.strip() and not i.strip().startswith("#")]
 
 setup(
     name="kubernaut",
@@ -8,11 +15,7 @@ setup(
     cmdclass=versioneer.get_cmdclass(),
     packages=find_packages(exclude=["tests"]),
     include_package_data=True,
-    install_requires=[
-        "click",
-        "requests",
-        "scout.py"
-    ],
+    install_requires=install_requirements,
     entry_points="""
         [console_scripts]
         kubernaut=kubernaut.cli:cli
